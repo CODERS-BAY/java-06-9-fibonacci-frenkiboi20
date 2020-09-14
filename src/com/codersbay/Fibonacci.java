dc02a363d0019e413f730d268d82d6ff360e8909
Jakob Penninger <jakob.penninger@gmx.net>
2020-09-14 18:22:48 +0200

    Add main

--- src/com/codersbay/Fibonacci.java
+++ src/com/codersbay/Fibonacci.java
@@ -1,9 +1,22 @@
 package com.codersbay;
 
+import java.util.Scanner;
+
 public class Fibonacci {
 
     public static void main(String[] args) {
-        System.out.println("Hello Fibonacci");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Welche Stelle soll berechnet werden: ");
+        long number = sc.nextLong();
+        System.out.println("An der " + number + ". Stelle lautet die Fibonacci-Zahl: " + fibonacci(number));
+        sc.close();
+    }
+
+    private static int fibonacci(long number) {
+
+        if (number == 1 || number == 2) return 1;
+        else return fibonacci(number - 1) + fibonacci(number - 2);
     }
 
 }
